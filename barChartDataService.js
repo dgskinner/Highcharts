bffe027684784d2da005ef9754b54d70b8c9840d
Duncan Skinner <skinnerduncan@gmail.com>
2016-04-20 11:18:24 -0700

    change format of mock bar chart data to (hopefully) better match actual data

--- barChartDataService.js
+++ barChartDataService.js
@@ -1,75 +1,136 @@
 app.service("barChartDataService", function () {
-    
+
     var _barChartData = {};
 
     var rawData = [
         {
-            name: "Standard 1",
-            value: 31
-        },
-        {
-            name: "Standard 2",
-            value: 46
-        },
-        {
-            name: "Standard 3",
-            value: 30
-        },
-        {
-            name: "Standard 4",
-            value: 22
-        },
+            buildingName: "Victoria County High School",
+            standards: [
+                {
+                    standardName: "Standard 1",
+                    value: 31
+                },
+                {
+                    standardName: "Standard 2",
+                    value: 46
+                },
+                {
+                    standardName: "Standard 3",
+                    value: 30
+                },
+                {
+                    standardName: "Standard 4",
+                    value: 22
+                },
 
-        {
-            name: "Standard 5",
-            value: 15
+                {
+                    standardName: "Standard 5",
+                    value: 15
+                },
+                {
+                    standardName: "Standard 6",
+                    value: 52
+                },
+                {
+                    standardName: "Standard 7",
+                    value: 42
+                },
+                {
+                    standardName: "Standard 8",
+                    value: 39
+                },
+                {
+                    standardName: "Standard 9",
+                    value: 12
+                },
+                {
+                    standardName: "Standard 10",
+                    value: 14
+                },
+                {
+                    standardName: "Standard 11",
+                    value: 3
+                },
+                {
+                    standardName: "Standard 12",
+                    value: 25
+                }
+            ]
         },
-        {
-            name: "Standard 6",
-            value: 52
-        },
-        {
-            name: "Standard 7",
-            value: 42
-        },
-        {
-            name: "Standard 8",
-            value: 39
-        },
-        {
-            name: "Standard 9",
-            value: 12
-        },
-        {
-            name: "Standard 10",
-            value: 14
-        },
-        {
-            name: "Standard 11",
-            value: 3
-        },
-        {
-            name: "Standard 12",
-            value: 25
+        {  
+            buildingName: "Madison East High School",
+            standards: [
+                {
+                    standardName: "Standard 1",
+                    value: 31
+                },
+                {
+                    standardName: "Standard 2",
+                    value: 42
+                },
+                {
+                    standardName: "Standard 3",
+                    value: 57
+                },
+                {
+                    standardName: "Standard 4",
+                    value: 22
+                },
+
+                {
+                    standardName: "Standard 5",
+                    value: 15
+                },
+                {
+                    standardName: "Standard 6",
+                    value: 48
+                },
+                {
+                    standardName: "Standard 7",
+                    value: 42
+                },
+                {
+                    standardName: "Standard 8",
+                    value: 39
+                },
+                {
+                    standardName: "Standard 9",
+                    value: 12
+                },
+                {
+                    standardName: "Standard 10",
+                    value: 4
+                },
+                {
+                    standardName: "Standard 11",
+                    value: 13
+                },
+                {
+                    standardName: "Standard 12",
+                    value: 7
+                }
+            ]
         }
     ];
 
-    var highest = rawData[0],
-        secondHighest = rawData[0],
-        lowest = rawData[0],
-        secondLowest = rawData[0];
+    var activeIndex = 0;
+    var schoolStandards = rawData[activeIndex].standards;
+    var highest = schoolStandards[0],
+        secondHighest = schoolStandards[0],
+        lowest = schoolStandards[0],
+        secondLowest = schoolStandards[0];
 
-    for (var i = 1; i < rawData.length; i++) {
-        if (rawData[i].value > highest.value) {
+    for (var i = 1; i < schoolStandards.length; i++) {
+        if (schoolStandards[i].value > highest.value) {
             secondHighest = highest;
-            highest = rawData[i];
-        } else if (rawData[i].value > secondHighest.value) {
-            secondHighest = rawData[i];
-        } else if (rawData[i].value < lowest.value) {
+            highest = schoolStandards[i];
+        } else if (schoolStandards[i].value > secondHighest.value) {
+            secondHighest = schoolStandards[i];
+        } else if (schoolStandards[i].value < lowest.value) {
             secondLowest = lowest;
-            lowest = rawData[i];
-        } else if (rawData[i].value < secondLowest.value) {
-            secondLowest = rawData[i];
+            lowest = schoolStandards[i];
+        } else if (schoolStandards[i].value < secondLowest.value) {
+            secondLowest = schoolStandards[i];
         }
     }
 
@@ -81,6 +142,7 @@ app.service("barChartDataService", function () {
             item.data = [item.value];
         });
     }
+    
     groomDataForHighcharts(highActivityDataset);
     groomDataForHighcharts(lowActivityDataset);
 
