92bfd3797b27c1f11297460ce7b2169b0617afc9
Duncan Skinner <skinnerduncan@gmail.com>
2016-04-26 13:52:20 -0700

    refresh working for low activity bar chart

--- highActivityBarChartController.js
+++ highActivityBarChartController.js
@@ -85,38 +85,11 @@ app.controller("barChartsController", function ($scope, barChartDataService) {
         }
     };
 
-    $scope.updateChartOptions = function () {
-        $scope.highActivityBarChartOptions.series = $scope.barChartData.highActivityDataset;
-        $scope.highActivityBarChartOptions.yAxis.max = $scope.barChartData.yAxisMaxHighActivity;
-    }
-
-    $scope.changeBuildingData = function (selectedBuilding) {
-        // barChartDataService.activeIndex = barChartDataService.activeIndex === 0 ? 1 : 0;
-        debugger
-        barChartDataService.getDataForBuilding(selectedBuilding.index);
-        $scope.updateChartOptions();
-        $scope.$broadcast("duncan", $scope.highActivityBarChartOptions);
-    };
-});
-
-// app.controller("barChartSelectController", function ($scope, barChartDataService) {
-//     $scope.dataset = barChartDataService.rawData;
-
-//     $scope.changeBuildingData = function (selectedBuilding) {
-//         // barChartDataService.activeIndex = barChartDataService.activeIndex === 0 ? 1 : 0;
-//         barChartDataService.getDataForBuilding(selectedBuilding.index);
-//     };
-// });
-
-app.controller("highActivityBarChartController", function($scope, globalHighchartOptions) {
-
-    // $scope.barChartData = barChartDataService;
-
-    $scope.barChartOptions = {
+    $scope.lowActivityBarChartOptions = {
         chart: {
             type: 'bar',
         },
-        colors: $scope.barChartData.highActivityColors,
+        colors: barChartDataService.lowActivityColors,
         title: {
             text: null
         },
@@ -153,7 +126,7 @@ app.controller("highActivityBarChartController", function($scope, globalHighchar
                 text: null
             },
             // 3/2 times the max data point
-            max: $scope.barChartData.yAxisMaxHighActivity
+            max: barChartDataService.yAxisMaxLowActivity
         },
         tooltip: {
             enabled: false
@@ -162,7 +135,7 @@ app.controller("highActivityBarChartController", function($scope, globalHighchar
             bar: {
                 dataLabels: {
                     enabled: true,
-                    format: "{series.standardName}{point.standardName}<br>{y} Activities",
+                    format: "{series.name}<br>{y} Activities",
                     padding: 18,
                     style: {
                         "fontSize": "18px",
@@ -179,31 +152,23 @@ app.controller("highActivityBarChartController", function($scope, globalHighchar
         credits: {
             enabled: false
         },
-        series: $scope.barChartData.highActivityDataset,
+        series: barChartDataService.lowActivityDataset,
         exporting: {
             enabled: false
         }
     };
 
-    $scope.chartOptions = angular.extend($scope.barChartOptions, globalHighchartOptions);
-
     $scope.updateChartOptions = function () {
-        $scope.chartOptions.series = $scope.barChartData.highActivityDataset;
-        $scope.chartOptions.yAxis.max = $scope.barChartData.yAxisMaxHighActivity;
+        $scope.highActivityBarChartOptions.series = $scope.barChartData.highActivityDataset;
+        $scope.highActivityBarChartOptions.yAxis.max = $scope.barChartData.yAxisMaxHighActivity;
+        $scope.lowActivityBarChartOptions.series = $scope.barChartData.lowActivityDataset;
+        $scope.lowActivityBarChartOptions.yAxis.max = $scope.barChartData.yAxisMaxLowActivity;
     }
 
-    // console.log($scope.chartOptions.series[0]);
-
-    // $scope.$watch(function () { return barChartDataService.activeIndex }, function (newVal, oldVal) {
-    //     // if (typeof newVal !== 'undefined') {
-    //     //     // $scope.taskList = uaProgressService.taskList;
-    //     //     console.log("new active Index: " + newVal);
-    //     //     $scope.barChartData = barChartDataService;
-    //     //     // console.log($scope.barChartData);
-    //     //     $scope.updateChartOptions();
-    //     //     debugger
-    //     // }
-    //     $scope.updateChartOptions();
-    //     $scope.$broadcast("duncan", $scope.chartOptions);
-    // });
+    $scope.changeBuildingData = function (selectedBuilding) {
+        barChartDataService.getDataForBuilding(selectedBuilding.index);
+        $scope.updateChartOptions();
+        $scope.$broadcast("highActivityBarChartRefresh", $scope.highActivityBarChartOptions);
+        $scope.$broadcast("lowActivityBarChartRefresh", $scope.lowActivityBarChartOptions);
+    };
 });
